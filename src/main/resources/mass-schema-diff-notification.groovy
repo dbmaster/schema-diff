427b7d1455676ebd78d27d754f23881f4b3ced37
vitalh <vitaly@branegy.com>
2014-06-23 14:35:19 +0300

    improvement logging add email notification sending

--- src/main/resources/mass-schema-diff-notification.groovy
+++ src/main/resources/mass-schema-diff-notification.groovy
@@ -3,13 +3,20 @@ import java.text.SimpleDateFormat
 import com.branegy.dbmaster.model.*
 import com.branegy.dbmaster.sync.api.*
 import com.branegy.email.EmailSender
+import org.apache.commons.io.*
+import javax.mail.Message.RecipientType;
 
 def fileToString = { file ->
     return IOUtils.toString(file.toURI().toURL(), Charsets.UTF_8)
 }
 
+def stringToFile = { file, content -> 
+    return FileUtils.writeStringToFile(file, content, Charsets.UTF_8)
+}
+
 Date versionA = null;
 Date versionB = null;
+Date version = new Date();
 
 def sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
 def configFile = new File(p_storage_folder + "/" + p_config_name);
@@ -17,7 +24,18 @@ if (configFile.exists() && configFile.isFile()){
    versionA = sdf.parse(fileToString(configFile));
 }
 
-print new MassSchemaDiffHistory(logger, versionA, versionB, dbm)
+String emailBody = new MassSchemaDiffHistory(logger, versionA, versionB, dbm)
     .getHistory(p_database_query,p_storage_folder)
+    
 
+stringToFile(configFile, sdf.format(version));
 
+String[] to = p_emails.split("[,;]");
+
+EmailSender email = dbm.getService(EmailSender.class);
+email.createMessage(to[0], "Mass schema diff result for ${sdf.format(version)}", "Please find database changes attached", true)
+email.addAttachment("changes.html", emailBody)
+for (int i=1; i<to.length; ++i){
+    email.addRecepient(RecipientType.TO, to[i]);
+}
+email.sendMessage();
--- src/main/resources/mass-schema-diff.groovy
+++ src/main/resources/mass-schema-diff.groovy
@@ -16,15 +16,10 @@ import com.thoughtworks.xstream.XStream
 
 import java.io.*
 import java.text.SimpleDateFormat
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.Charsets
 
 import com.branegy.dbmaster.sync.api.SyncService
-
-modelService = dbm.getService(ModelService.class)
-syncService = dbm.getService(SyncService.class)
-invService = dbm.getService(InventoryService.class)
-
-String currentVersion = new SimpleDateFormat("yyyyMMdd_HHmm").format(new Date())
-def sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
 
 def xstream = new XStream();
 
@@ -33,7 +28,12 @@ def saveObjectToFile = { object, file_name ->
     file.getParentFile().mkdirs();
     def outStream = new FileOutputStream(file)
     try {
-        xstream.toXML(object,outStream)
+        if (object instanceof String){
+            IOUtils.write((String)object, outStream, Charsets.UTF_8);
+        } else {
+            xstream.toXML(object,outStream)
+        }
+        outStream.flush();
     } finally {
         outStream.close()
     }
@@ -47,7 +47,7 @@ def loadModel = { server_name, db_name ->
     Model model = null;
     logger.debug("Loading model from file ${file_name}")
     if(file.exists() && !file.isDirectory()) { 
-         model =  xstream.fromXML(file);
+         model = xstream.fromXML(file);
     } else {
         logger.debug("File does not exist ${file_name}")
     }
@@ -76,7 +76,11 @@ def saveErrorMessage = { server_name, db_name,  message ->
 }
 
 
+modelService = dbm.getService(ModelService.class)
+syncService = dbm.getService(SyncService.class)
+invService = dbm.getService(InventoryService.class)
 
+def sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
 
 for (Database db:invService.getDatabaseList(new QueryRequest(p_database_query))) {
     if (db.getDatabaseName().equalsIgnoreCase("tempdb")) {
@@ -91,16 +95,23 @@ for (Database db:invService.getDatabaseList(new QueryRequest(p_database_query)))
         logger.debug("Time is ${sdf.format(version)} (fetching new model)")
         
         def targetModel = modelService.fetchModel(db.getServerName(), options);
+        
+        logger.debug("Time is ${sdf.format(new Date())} (loading storedModel)")
         def sourceModel = loadModel(db.getServerName(), db.getDatabaseName());
         if (sourceModel == null){
+            logger.debug("Time is ${sdf.format(new Date())} (saving model)")
             saveModel(db.getServerName(), targetModel);
+            logger.info("${db.getServerName()}.${db.getDatabaseName()}: new database")
         } else {
             SyncSession syncSession = modelService.compareModel(sourceModel, targetModel);
             if (syncSession.getSyncResult().getChangeType() != com.branegy.dbmaster.sync.api.SyncPair.ChangeType.EQUALS){
+                logger.debug("Time is ${sdf.format(new Date())} (saving model)")
                 saveModel(db.getServerName(), targetModel);
                 
+                logger.debug("Time is ${sdf.format(new Date())} (generate sync)")
                 String diff = syncService.generateSyncSessionPreviewHtml(syncSession, true);
                 saveDiff(db.getServerName(), db.getDatabaseName(), diff, version);
+                logger.info("${db.getServerName()}.${db.getDatabaseName()}: data structure changes found")
             } else {
                 def file_name = p_storage_folder + "/" + db.getServerName() + "/" + db.getDatabaseName() + "/model.dat"
                 new File(file_name).setLastModified(version.getTime())
@@ -114,85 +125,3 @@ for (Database db:invService.getDatabaseList(new QueryRequest(p_database_query)))
         println "<div>${db.getServerName()}.${db.getDatabaseName()} error: ${e.getMessage()}</div>"
     } 
 }
-
-
-/*
-
-for (Database db:invService.getDatabaseList(new QueryRequest(p_database_query))) {
-    if (db.getDatabaseName().equalsIgnoreCase("tempdb")) {
-        continue;
-    }
-    def modelName = db.getServerName()+"_"+db.getDatabaseName()
-    try {
-        logger.debug("Time is ${sdf.format(new Date())} (loading storedModel)")
-        def model = loadModel(db.getServerName(), db.getDatabaseName())
-
-        if (model == null) {
-            RevEngineeringOptions options = new RevEngineeringOptions()
-            options.database = db.getDatabaseName()
-        
-            logger.debug("Time is ${sdf.format(new Date())} (fetching new model)")
-            def newModel = modelService.fetchModel(db.getServerName(), options)
-            
-            println "<div>New database: ${db.getServerName()}.${db.getDatabaseName()}</div>"
-        
-            newModel.setName(modelName)
-            newModel.setVersion(currentVersion)
-
-            logger.debug("Time is ${sdf.format(new Date())} (saving model)")
-            saveModel(db.getServerName(), newModel)
-            logger.info("${db.getServerName()}.${db.getDatabaseName()}: new database")
-            // emailWriter.append("<div>New database: ${db.getDatabaseName()}</div>")
-        } else {
-            logger.debug("Time is ${sdf.format(new Date())} (fetching model)")
-            def targetModel = modelService.fetchModel(model.getConnectionName(), model.getOptions())
-            logger.debug("Time is ${sdf.format(new Date())} (comparing model)")
-            def syncSession = modelService.compareModel(model, targetModel)
-            syncSession.setParameter("longText", "Procedure.Source;View.Source;Function.Source")
-
-            if (syncSession.getSyncResult().getChangeType() != ChangeType.EQUALS){
-                logger.debug("Time is ${sdf.format(new Date())} (generate sync)")
-                def preview = invService.generateDBSyncPreview(syncSession)
-                println "<div>Changes: ${db.getServerName()}.${db.getDatabaseName()} since ${model.getVersion()}</div>"
-                println "<div>${preview}</div>"
-            
-               // emailWriter.append("""<div>Changes detected in ${db.getServerName()}.${db.getDatabaseName()} 
-                //                      (${currentVersion} vs ${model.getVersion()} )</div>
-                 //                     <div>${preview}</div>""");
-            
-                targetModel.setVersion(currentVersion)
-                targetModel.setName(modelName)
-                
-                //def file_name = "${p_storage_folder}/${db.getServerName()}_${db.getDatabaseName()}_diff_${currentVersion}.dat"
-                // saveObjectToFile(syncSession, file_name)
-                // http://stackoverflow.com/questions/603013/dumping-a-java-objects-properties
-                // XStream xstream = new XStream(new Sun14ReflectionProvider(new FieldDictionary(new ImmutableFieldKeySorter())), new DomDriver("utf-8"));
-                
-                def file_name = "${p_storage_folder}/${db.getServerName()}_${db.getDatabaseName()}_diff_${currentVersion}.html"
-                new File(file_name).write(preview)
-
-                // saveModel(db.getServerName(), targetModel)
-                logger.info("${db.getServerName()}.${db.getDatabaseName()}: data structure changes found")
-            } else {
-                println "<div>${db.getServerName()}.${db.getDatabaseName()}: No data structure changes since ${model.getVersion()} </div>"
-            
-                // model.setLastSynch(targetModel.getLastSynch())
-                // saveModel(db.getServerName(), model)
-                logger.info("${db.getServerName()}.${db.getDatabaseName()}: No changes");
-            }
-        }
-    } catch (Exception e) {
-        logger.error("${db.getServerName()}.${db.getDatabaseName()}: Error occurs", e)
-        println "<div>${db.getServerName()}.${db.getDatabaseName()} error: ${e.getMessage()}</div>"
-        // emailWriter.append("<div>${db.getServerName()}.${db.getDatabaseName()} error: ${e.getMessage()}</div>");
-    }
-}
-
-
-if (emailWriter.length() != 0) {
-    EmailSender sender = dbm.getService(EmailSender.class);
-    sender.createMessage(p_email, "Mass schema diff result for ${currentVersion}", "Please find database changes attached", true)
-    sender.addAttachment("changes.html", emailWriter.toString())
-    sender.sendMessage()
-}
-*/
