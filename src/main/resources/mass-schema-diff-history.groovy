57172e7be5829f1f3e02a684607a5b4ff0bc75b5
vitalh <vitaly@branegy.com>
2014-08-27 19:19:10 +0300

    fix spelling

--- src/main/resources/mass-schema-diff-history.groovy
+++ src/main/resources/mass-schema-diff-history.groovy
@@ -5,6 +5,6 @@ def p_storage_folder = com.branegy.util.DataDirHelper.getDataDir()+"/schema-diff
 String result = new MassSchemaDiffHistory(logger, p_from_date, p_to_date, dbm)
     .getHistory(p_database_query,p_storage_folder);
 
-print result.isEmpty()?"No changes was found":result;
+print result.isEmpty()?"No changes were found":result;
 
 
