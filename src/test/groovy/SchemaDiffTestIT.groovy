c03bd1b8651dfc62ed7feb961f20e2f2b36978ff
vitalh <vitaly@branegy.com>
2015-08-20 11:51:17 +0300

    add test

--- src/test/groovy/SchemaDiffTestIT.groovy
+++ src/test/groovy/SchemaDiffTestIT.groovy
@@ -11,6 +11,7 @@ import com.branegy.inventory.api.InventoryService
 import com.branegy.dbmaster.database.api.ModelService
 import com.branegy.dbmaster.model.*
 import com.branegy.dbmaster.sync.api.*
+import com.branegy.dbmaster.sync.api.SyncPair.ChangeType;
 import com.branegy.service.connection.api.ConnectionService
 import com.branegy.service.connection.model.DatabaseConnection
 import com.branegy.dbmaster.connection.JDBCDialect
@@ -26,6 +27,8 @@ import io.dbmaster.testng.BaseToolTestNGCase;
 
 import java.io.File;
 import java.sql.Connection
+
+import javax.persistence.EntityManager;
 
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -132,6 +135,105 @@ public class SchemaDiffTestIT extends BaseServiceTestNGCase{
             if (writer!=null) {
                 writer.print("</div>");
                 writer.print("</body></html>");
+                writer.flush();
+            }
+            IOUtils.closeQuietly(os);
+        }
+    }
+    
+    @Test
+    public void testModelSync() {
+        def targetDB =       getTestProperty("schema-diff.target_db");
+        def connectionName = getTestProperty("schema-diff.connection");
+        def testFolder =     getTestResourcesDir().getPath();
+        def testReportFile = new File(getBuildDir(),"/model-sync-report.html");
+        
+        OutputStream os = null;
+        PrintWriter writer = null;
+        try {
+            os = FileUtils.openOutputStream(testReportFile);
+            writer = new PrintWriter(os);
+            writer.print("<!DOCTYPE html>");
+            writer.print("<html><head>");
+            writer.print("<style>");
+            writer.print(IOUtils.toString(DirectHmltDataPresenter.class.getResource("extra.css"), Charsets.UTF_8));
+            writer.print("</style>");
+            writer.print("</head><body>");
+            writer.print("<div>");
+            
+            Model originalModel = null;
+            def dir = new File("${testFolder}/tests/queries")
+            dir.eachFileRecurse (FileType.FILES) { file ->
+                println ("Processing ${file.name}")
+                writer.println("------------------------- Processing test case for file: ${file.name}")
+                writer.println("<pre>")
+                writer.println(file.text)
+                writer.println("</pre>")
+                writer.println("------------------------- Sync table --------------------------------")
+                
+                println("Cleanup/initiate database with  000_setup.sql")
+                runSQL(connectionName, targetDB, "${testFolder}/000_cleanup.sql")
+                runSQL(connectionName, targetDB, "${testFolder}/000_setup.sql")
+            
+                RevEngineeringOptions options = new RevEngineeringOptions();
+                options.database = targetDB
+                options.importProcedures = true
+                options.importViews  = true
+            
+                if (originalModel == null) {
+                    println("Loading initial model")
+                    originalModel = modelService.fetchModel(connectionName, options)
+                }
+                
+                // model before changes
+                def tempModel = modelService.fetchModel(connectionName, options)
+            
+                runSQL(connectionName, targetDB, file.canonicalPath)
+            
+                def changedModel = modelService.fetchModel(connectionName, options)
+                def syncSession = modelService.compareModel(originalModel, changedModel)
+                
+                def template = "/preview-model-generator.groovy"
+                def preview = syncService.generateSyncSessionPreviewHtml(template, syncSession, true)
+            
+                writer.println("<div>" + preview + "</div>")
+                
+                //----------------------------
+                writer.println("------------------------- Apply test -------------------------------- ")
+                println("Testing apply changes")
+                tempModel.setName("tmp_model");
+                tempModel.setVersion("tmp_model");
+                tempModel = modelService.createModel(tempModel, null)
+                EntityManager em = com.branegy.util.InjectorUtil.getInstance(EntityManager.class);
+                em.getTransaction().commit();
+                em.getTransaction().begin();
+                
+                syncSession = modelService.compareModel(tempModel, changedModel)
+                syncSession.applyChanges();
+                
+                tempModel = modelService.saveModel(tempModel, com.branegy.dbmaster.gwt.module.database.handler.ModelBackendService.FETCH_CONNECTION_PROJECT);
+                
+                em.getTransaction().commit();
+                em.getTransaction().begin();
+                
+                syncSession = modelService.compareModel(tempModel, changedModel);
+                if (syncSession.getSyncResult().getChangeType() == ChangeType.EQUALS){
+                    writer.println("<div>Apply test is passed</div>")
+                } else {
+                    preview = syncService.generateSyncSessionPreviewHtml(template, syncSession, true)
+                    writer.println("<div>Apply test is NOT passed</div>")
+                    writer.println("<div>" + preview + "</div>");
+                }
+                
+                modelService.deleteModel(tempModel.getId());
+                em.getTransaction().commit();
+                em.getTransaction().begin();
+                em.clear();
+            }
+        } finally {
+            if (writer!=null) {
+                writer.println("</div>");
+                writer.println("</body></html>");
                 writer.flush();
             }
             IOUtils.closeQuietly(os);
