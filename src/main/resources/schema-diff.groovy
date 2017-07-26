54bdf698177921d62a3249256222aa8786e4cda2
vitalh <vitaly@branegy.com>
2017-07-26 11:44:41 +0300

    Add 'Ignore Objects' parameter #21

--- src/main/resources/schema-diff.groovy
+++ src/main/resources/schema-diff.groovy
@@ -2,14 +2,15 @@ import com.branegy.dbmaster.database.api.ModelService
 import com.branegy.dbmaster.model.*
 import io.dbmaster.sync.*
 import com.branegy.dbmaster.sync.api.SyncService
+import java.util.regex.*
 
 modelService = dbm.getService(ModelService.class)
 
-def source_server =   p_source_database.split("\\.")[0]
-def source_database = p_source_database.split("\\.")[1]
+def source_server =   p_source_database.split("\\.",2)[0]
+def source_database = p_source_database.split("\\.",2)[1]
 
-def target_server =   p_target_database.split("\\.")[0]
-def target_database = p_target_database.split("\\.")[1]
+def target_server =   p_target_database.split("\\.",2)[0]
+def target_database = p_target_database.split("\\.",2)[1]
 
 
 RevEngineeringOptions source_options = new RevEngineeringOptions()
@@ -29,9 +30,123 @@ target_options.importProcedures = true
 logger.info("Loading target database")
 targetModel = modelService.fetchModel(target_server, target_options)
 
-sourceModel  = new ModelConverter(logger).
+/*sourceModel  = new ModelConverter(logger).
   convertModel(sourceModel, targetModel.getCustomData("dialect"), 
                             targetModel.getCustomData("dialect_version"))
+*/
+
+def ignoreObjects(String ignoreObjects, Model source,Model target) {
+    if (ignoreObjects != null) {
+        Map<String,Set<Matcher>> ignore = [:] // type <-> regexp
+        
+        def getOrCreate = { map, key ->
+            def result = map[key]
+            if (result == null) {
+                result = map[key] = [:];
+            }
+            return result;
+        }
+        
+        def toMatcher = { String query ->  
+            StringBuilder pattern = new StringBuilder(query.length()*2);
+            def start = 0;
+            for(int j = 0; j < query.length(); ++j){
+               switch (query.charAt(j)) {
+                   case '*':
+                   pattern.append(Pattern.quote(query.substring(start,j)));
+                   pattern.append(".*");
+                   start = j+1;
+                   break;
+                   case '?':
+                   pattern.append(Pattern.quote(query.substring(start,j)));
+                   pattern.append('.');
+                   start = j+1;
+                   break;
+               }
+            }
+            if (start == 0){
+                pattern.append(Pattern.quote(query));
+            }
+            return Pattern.compile(pattern.toString(),Pattern.CASE_INSENSITIVE).matcher("");
+        }
+        
+        Matcher tokenMatcher = Pattern.compile("(table|view|procedure|function)\\s*:\\s*(.+)",Pattern.CASE_INSENSITIVE).matcher("");
+        ignoreObjects.split(",").each{ token ->
+            token = token.trim();
+            if (tokenMatcher.reset(token).matches()) { // by type
+                def type  = tokenMatcher.group(1).toLowerCase();
+                def value = tokenMatcher.group(2);
+                getOrCreate(ignore,type).put(toMatcher(value),token);
+            } else { // any type
+                def matcher = toMatcher(token);
+                getOrCreate(ignore,"table").put(matcher,token);
+                getOrCreate(ignore,"view").put(matcher,token);
+                getOrCreate(ignore,"procedure").put(matcher,token);
+                getOrCreate(ignore,"function").put(matcher,token);
+            }
+        }
+        
+        def filter = { Map<Matcher,String> matchers, Model parent, String type, String extraInfo ->
+           if (matchers != null) {
+               def toRemove = [];
+               def objects;
+               switch (type) {
+                   case "table": objects = parent.tables; break;
+                   case "view": objects = parent.views; break;
+                   case "procedure": objects = parent.procedures; break;
+                   case "function": objects = parent.functions; break;
+               }
+               for (object in objects) {
+                  def name = object.getName();
+                  for (e in matchers.entrySet()) {
+                      def matcher = e.key
+                      matcher.reset(name);
+                      if (matcher.matches()) {
+                          toRemove.add(object);
+                          logger.debug("Ingore {} {} {} by {}",extraInfo,type,name,e.value);
+                      }
+                  }
+               }
+               switch (type) {
+                   case "table":
+                       for (object in toRemove) {
+                           parent.removeTable(object);
+                       }
+                       break;
+                   case "view":
+                       for (object in toRemove) {
+                           parent.removeView(object);
+                       }
+                       break;
+                   case "procedure":
+                       for (object in toRemove) {
+                           parent.removeProcedure(object);
+                       }
+                       break;
+                   case "function":
+                       for (object in toRemove) {
+                           parent.removeFunction(object);
+                       }
+                       break;
+               }
+           }
+        };
+        
+        filter(ignore["table"],source,"table","source");
+        filter(ignore["table"],target,"table","target");
+        
+        filter(ignore["view"],source,"view","source");
+        filter(ignore["view"],target,"view","target");
+        
+        filter(ignore["function"],source,"function","source");
+        filter(ignore["function"],target,"function","target");
+        
+        filter(ignore["procedure"],source,"procedure","source");
+        filter(ignore["procedure"],target,"procedure","target");
+    }
+}
+
+ignoreObjects(p_ignore_objects,sourceModel,targetModel);
 
 logger.info("Comparing databases")
 def sync_session = modelService.compareModel(sourceModel, targetModel)
